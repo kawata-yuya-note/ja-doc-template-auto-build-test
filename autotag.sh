206fb67156a906c895fd0cce13fc344ce28ac3de
kawata-yuya <kwtyuya@gmail.com>
2025-12-01 17:32:02 +0900

    Add script for creating and pushing new tags

---
+++ autotag.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Source - https://stackoverflow.com/a
+# Posted by timhc22, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-01, License - CC BY-SA 4.0
+
+
+# https://github.com/unegma/bash-functions/blob/main/update.sh
+
+VERSION=""
+
+#get parameters
+while getopts v: flag
+do
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
+done
+
+#get highest tag number, and add v0.0.0 if doesn't exist
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+if [[ $CURRENT_VERSION == '' ]]
+then
+  CURRENT_VERSION='v0.0.0'
+fi
+echo "Current Version: $CURRENT_VERSION"
+
+
+#replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+
+#get number parts
+VNUM1=`echo ${CURRENT_VERSION_PARTS[0]} | sed -e 's/[^0-9]//g'`
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+
+if [[ $VERSION == 'major' ]]
+then
+  VNUM1=$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
+else
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
+fi
+
+#create new tag
+NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+
+read -p "Do you really updating $VERSION to $NEW_TAG (y/n)" YN_LOADSETTING
+if [ "${YN_LOADSETTING}" != "y" ]; then
+  echo "bye"
+  exit 1
+fi
+echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+    git tag $NEW_TAG -m "Version $VNUM1.$VNUM2.$VNUM3"
+    echo "Tagged with $NEW_TAG"
+    git push --tags
+else
+    echo "Already a tag on this commit"
+fi
+
+exit 0
